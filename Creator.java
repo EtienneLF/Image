75b51cddce11ed7f07b79b6e96178f4b7af3dde2
EtienneLF <51823054+EtienneLF@users.noreply.github.com>
2022-10-20 22:45:07 -0400

    First Commit

---
+++ Creator.java
@@ -0,0 +1,14 @@
+public class Creator {
+
+    public static void main(String[] args) {
+        String folderPath = "Entry/Folder";
+        String imagePath = "Entry/Test.png";
+        String resultPath = "Exit/Result.png";
+
+        if (args.length == 0) {
+            System.out.println("Using default settings");
+        }
+
+        System.out.println("");
+    }
+}
