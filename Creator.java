f5fbb8240cb9ad1412079705cd390b0e624a749f
EtienneLF <51823054+EtienneLF@users.noreply.github.com>
2022-10-20 22:50:35 -0400

    Print

--- Creator.java
+++ Creator.java
@@ -1,3 +1,5 @@
+import Source.Image;
+
 public class Creator {
 
     public static void main(String[] args) {
@@ -5,10 +7,21 @@ public class Creator {
         String imagePath = "Entry/Test.png";
         String resultPath = "Exit/Result.png";
 
+        System.out.println("--------------------------------------------------------------");
+
         if (args.length == 0) {
             System.out.println("Using default settings");
         }
-
         System.out.println("");
+        System.out.println("Starting with parameters:" + "\n" +
+                "Folder path: " + folderPath + "\n" +
+                "Image path: " + imagePath + "\n" +
+                "Result path: " + resultPath + "\n");
+
+        Image image = new Image(imagePath, folderPath, resultPath);
+        image.createImage();
+
+        System.out.println("End of program");
+        System.out.println("--------------------------------------------------------------");
     }
 }
--- Source/Image.java
+++ Source/Image.java
@@ -1,5 +1,41 @@
 package Source;
 
 public class Image {
-    
+    private String imagePath;
+    private String folderPath;
+    private String resultPath;
+
+    public Image(String imagePath, String folderPath, String resultPath) {
+        this.imagePath = imagePath;
+        this.folderPath = folderPath;
+        this.resultPath = resultPath;
+    }
+
+    public void createImage() {
+        System.out.println("Creating image");
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getFolderPath() {
+        return folderPath;
+    }
+
+    public void setFolderPath(String folderPath) {
+        this.folderPath = folderPath;
+    }
+
+    public String getResultPath() {
+        return resultPath;
+    }
+
+    public void setResultPath(String resultPath) {
+        this.resultPath = resultPath;
+    }
 }
